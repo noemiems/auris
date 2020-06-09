46d1a95407d8b0735097a90feffef457e095a4ca
noemiems <59896235+noemiems@users.noreply.github.com>
2020-06-09 15:05:49 -0500

    Update auris.py

--- auris.py
+++ auris.py
@@ -12,6 +12,7 @@ line = "XXX" #Start the line
 wordlist = [] #Empty list
 freqfilmlist = [] #Empty list
 freqbooklist = [] #Empty list
+ipalist = []
 
 while line: #As long as there are lines
     line = lexique.readline() #Read each line of the document
@@ -23,16 +24,19 @@ while line: #As long as there are lines
         wordlist.append(word) #Append the word stored in variable "word" to the list "wordlist"
         freqfilmlist.append(freqfilm) #Append the word stored in variable "freqfilm" to the list "freqfilmlist"
         freqbooklist.append(freqbook) #Append the word stored in variable "freqbook" to the list "freqbooklist"
+        wordipa = line.split(',')[23] #Split the line at comas and store the second word of the line in a variable called 'word'
+        ipalist.append(wordipa) #Append the word stored in variable "word" to the list "wordlist"
 
-#with open("wordlist.txt", "w") as output: #Create the document wordlist.txt based on the list wordlist
-#    output.write(wordlist)))
-
-epi = epitran.Epitran('fra-Latn', cedict_file='wordlist.txt') #Apply the epitran module to our newly created document wordlist.txt
-ipalist = [] #Empty list to store the words in their IPA transcriptions
-lenwordlist = len(wordlist) #Store the length of the wordlist (integer) into the variable "lenwordlist"
-for item in range(lenwordlist): #loop through the wordlist and transcribes each word into its IPA translation
-    ipaword = epi.transliterate((wordlist[item])) #Store the IPA word into the variable "ipaword"
-    ipalist.append(ipaword) #Append the word from ipaword into ipalist
+for x in range(len(ipalist)):
+    def ipa(a, b):
+        if a in ipalist[x]:
+            ipalist[x] = re.sub(a, b, ipalist[x])
+    ipa('E', 'ɛ')
+    ipa('O', 'ɔ')
+    ipa('R', 'r')
+    ipa('Z', 'ʒ')
+    ipa('°', 'ə')
+    ipa('9', 'œ')
 
 lista = [] #emptylist
 listb = [] #emptylist
@@ -41,14 +45,15 @@ listg = []
 
 userinputa = input('IPA character = ') #User inputs the first IPA character to be compared
 userinputb = input('IPA character 2 = ') #User inputs the second IPA character to be compared
+userinputc = input('How many syllables max = ')
 
 for x in range(len(ipalist)): #Loop through the ipalist to look for the user's IPA character 1
     if userinputa in ipalist[x]: #Whenever the computer encounters a word that contains the IPA character
-        if len(re.findall('[aeiouyɛɔ̃ɔœ̃ɛ̃əâìjɥɑáàé]', ipalist[x])) == 1:
+        if len(re.findall('[-]', ipalist[x])) < int(userinputc)-1:
             lista.append(ipalist[x]) #It will append it to lista (Name for the list not great)
             listf.append(wordlist[x])
     if userinputb in ipalist[x]: #Same process for the second character requested by the user
-        if len(re.findall('[aeiouyɛɔ̃ɔœ̃ɛ̃əâìjɥɑáàé]', ipalist[x])) == 1:
+        if len(re.findall('[-]', ipalist[x])) < int(userinputc)-1:
             listb.append(ipalist[x]) #It is saved in another list called listb (Again the name is not great)
             listg.append(wordlist[x])
 
@@ -91,10 +96,8 @@ for x in range(len(words)):
             y = dict[words[x]]
             z = dico[wordsb[x]]
             print(y, z)
-            listc.append(y) 
+            listc.append(y)
             listc.append(z)
-
-print(listc)
 
 with open("bigram.txt", "w") as output: #This code is if you want to get the output as a txt file on your computer
     output.write("\n".join(" ".join(row) for row in zip(*[iter(listc)]*2))) #It will output a bigram
