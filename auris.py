78d57d56add1dc5d5b0722185c9a43b41e1479c4
noemiems <59896235+noemiems@users.noreply.github.com>
2020-07-24 16:09:56 -0500

    Update auris.py

--- auris.py
+++ auris.py
@@ -8,6 +8,7 @@ lexique = open("lexique.txt")
 line = "XXX"
 wordlist = []
 firstlist = []
+alist = []
 
 while line:
     line = lexique.readline()
@@ -30,25 +31,17 @@ while line:
         if '9' in wordipa:
             wordipa = re.sub('9', 'œ', wordipa)
         word = line.split(',')[1]
+        alist.append([wordipa, word])
         freqfilm = line.split(',')[7]
         if '.' in freqfilm:
             freq = float(freqfilm)
             if freq >= 0: #Change this for frequency threshold
                 if len(re.findall('[-]', wordipa)) < 5 : #Change this for word length
                     if 'y' in wordipa: #Change this for character to compare
-                        saved = wordipa
                         savedb = word
-                        new = re.sub('y', 'u', saved) #Change this for character to compare
+                        new = re.sub('y', 'u', wordipa) #Change this for character to compare
                         firstlist.append([new, savedb])
-                    if 'u' in wordipa: #Change this for character to compare
-                        d = {}
-                        d[freqfilm] = [word, wordipa]
-                        wordlist.append(d)
-
-for dict in range(len(wordlist)): #It prints each combination twice, I am not sure why
-    for key in wordlist[dict]:
-        for z in range(len(firstlist)):
-            for item in wordlist[dict][key]:
-                if firstlist[z][0] == wordlist[dict][key][1]:
-                    wd = wordlist[dict][key][0]
-                    print(firstlist[z][0])
+                        for x in range(len(alist)):
+                            for item in alist[x]:
+                                if new == item:
+                                    print(savedb, alist[x][1])
